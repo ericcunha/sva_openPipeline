a62b5499111f81f37a1f1a5337cccb35542d47ea
Eric Cunha <emcunha77@gmail.com>
2020-04-12 12:28:15 -0400

    changed startup procedure

--- sva_openPipeline/__init__.py
+++ sva_openPipeline/__init__.py
@@ -1,8 +1,10 @@
 import os
 import maya.mel as mel
 
-# get the directory the script is running from
-op_path = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
-mel.eval('source "{op_path}/openPipeline.mel"'.format(op_path=op_path))
-# tell openPipeline where to find the openPipeline script folder
-mel.eval('openPipeline("{}","{}")'.format(op_path, op_path))
+
+def start():
+    # get the directory the script is running from
+    op_path = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
+    mel.eval('source "{op_path}/openPipeline.mel"'.format(op_path=op_path))
+    # tell openPipeline where to find the openPipeline script folder
+    mel.eval('openPipeline("{}","{}")'.format(op_path, op_path))
